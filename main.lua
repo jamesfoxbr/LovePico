f28180f048f57a4a1a858a46644206ed0460589f
jamesfoxbr <jamesfoxbr@hotmail.com>
2024-05-24 18:17:44 -0300

    added comments

--- main.lua
+++ main.lua
@@ -15,9 +15,11 @@ function love.draw()
     startCanvas(color.black)
     for y=0, 128 do
         for x=0, 128 do
-            pset(math.random(15), x, y)
+            pset(math.random(15), x, y) -- All math functions from pico exist in lua just add "math." before like the math.random() example
         end
     end
+    -- colors os the sprite can be anything not just pico colors unless you decide to use the pico palette
+    -- you need load the image first before draw here (see love.load function above)
     spr(Annikka, 8, 1)
     
     
